9f0ac512680b4a1546b2a9abd0ed1f592de7d196
Andrew Snodgrass <asnodgrass@anypresence.com>
2016-01-31 19:20:38 -0700

    Added random songs

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"errors"
 	"flag"
+	"fmt"
 	"io"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -19,6 +22,32 @@ import (
 )
 
 var file = flag.String("file", "", "file to hash")
+var seed = flag.Int64("seed", 0, "random seed for song")
+
+type RandReader struct {
+	size int
+	rnd  *rand.Rand
+}
+
+func NewRandReader(size int, seed int64) *RandReader {
+	return &RandReader{
+		size: size,
+		rnd:  rand.New(rand.NewSource(seed)),
+	}
+}
+
+func (rr *RandReader) Read(p []byte) (n int, err error) {
+	for i := range p {
+		p[i] = byte(rr.rnd.Int())
+		rr.size--
+		n++
+		if rr.size == 0 {
+			err = errors.New("End of random stream")
+			return
+		}
+	}
+	return
+}
 
 func main() {
 	flag.Parse()
@@ -40,6 +69,26 @@ func main() {
 		ks.Sound(&wav)
 
 		out, err := os.Create(strings.TrimSuffix(*file, filepath.Ext(*file)) + ".wav")
+		if err != nil {
+			log.Fatal(err)
+		}
+		io.Copy(out, wav.Reader())
+		return
+	}
+
+	if *seed != 0 {
+		in := NewRandReader(2*1024*1024, *seed)
+		h := hasher.New(in)
+		sng := h.Hash()
+
+		wav := wave.New(wave.Stereo, 22000)
+		ks := karplus.Song{
+			Song:         *sng,
+			SamplingRate: 22000,
+		}
+		ks.Sound(&wav)
+
+		out, err := os.Create(fmt.Sprintf("%v.wav", *seed))
 		if err != nil {
 			log.Fatal(err)
 		}

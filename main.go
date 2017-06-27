8b3d059dcc04da13e0d58e8720f606bb9d9081d1
Andrew Snodgrass <asnodgrass@anypresence.com>
2017-06-27 15:48:55 -0600

    Use burrows wheeler transform

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"errors"
 	"flag"
 	"fmt"
 	"io"
 	"log"
+	"math"
 	"math/rand"
 	"net/http"
 	"os"
@@ -49,6 +51,91 @@ func (rr *RandReader) Read(p []byte) (n int, err error) {
 	return
 }
 
+func inverse(buffer []byte) {
+	length := len(buffer)
+	input, major, minor := make([]byte, length), [256]int{}, make([]int, length)
+	for k, v := range buffer {
+		input[k], minor[k], major[v] = v, major[v], major[v]+1
+	}
+
+	sum := 0
+	for k, v := range major {
+		major[k], sum = sum, sum+v
+	}
+
+	j := length - 1
+	for k, _ := range input {
+		for minor[k] != -1 {
+			buffer[j], j, k, minor[k] = input[k], j-1, major[input[k]]+minor[k], -1
+		}
+	}
+}
+
+type StructuredReader struct {
+	*bytes.Reader
+}
+
+func NewStructuredReader(size int, seed int64) *StructuredReader {
+	rnd := rand.New(rand.NewSource(seed))
+	samples := make([]byte, 4)
+	for i := range samples {
+		samples[i] = byte(rnd.Int())
+	}
+	j, out := 0, make([]byte, size)
+	for i := range out {
+		out[i] = samples[j]
+		j++
+		if j >= len(samples) {
+			inverse(samples)
+			j = 0
+		}
+	}
+	return &StructuredReader{bytes.NewReader(out)}
+}
+
+type HoloReader struct {
+	*bytes.Reader
+}
+
+func NewHoloReader(size int, seed int64) *HoloReader {
+	rnd := rand.New(rand.NewSource(seed))
+	out := make([]byte, size)
+	for i := range out {
+		sample := math.Abs(rnd.NormFloat64() * 8)
+		if sample > 256 {
+			sample = 256
+		}
+		out[i] = byte(sample)
+	}
+
+	//move to front
+	nodes := [256]byte{}
+	var first byte
+
+	for node, _ := range nodes {
+		nodes[node] = uint8(node) + 1
+	}
+
+	for i, symbol := range out {
+		var node, next byte
+		moveToFront := symbol != 0
+		for next = first; symbol > 0; node, next = next, nodes[next] {
+			symbol--
+		}
+
+		if moveToFront {
+			first, nodes[node], nodes[next] = next, nodes[next], first
+		}
+
+		out[i] = next
+	}
+
+	//burrows wheeler
+	inverse(out)
+
+	return &HoloReader{bytes.NewReader(out)}
+}
+
 func main() {
 	flag.Parse()
 
@@ -77,7 +164,7 @@ func main() {
 	}
 
 	if *seed != 0 {
-		in := NewRandReader(2*1024*1024, *seed)
+		in := NewStructuredReader(2*1024*1024, *seed)
 		h := hasher.New(in)
 		sng := h.Hash()
 
